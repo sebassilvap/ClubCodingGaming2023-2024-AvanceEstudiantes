aaa8e5e6500bfdae4a2c8507ec51660e76b34de9
Sebastian Silva <sebas.silva.p@gmail.com>
2023-10-29 18:36:33 +0100

    CLASES EXTRA - 29.10.2023-18:36pm

--- 21-Operadores-Logicos/operadores_logicos.py
+++ 21-Operadores-Logicos/operadores_logicos.py
@@ -7,9 +7,9 @@
 '''
 Operador |  Ejemplo  |  Descripción
 ----------------------------------------------------------------
-not      |  X and Y  |  True si todos son True
-and      |  X or Y   |  Basta que 1 sea True, resultado es True
-or       |  not X    |  Niega al booleano
+not      |  not X    |  Niega al booleano
+and      |  X and Y  |  True si todos son True 
+or       |  X or Y   |  Basta que 1 sea True, resultado es True
 '''
 # =================================================================
 
@@ -19,7 +19,25 @@ print('\n1) Operador not')
 # - realiza una negación lógica
 # - niega / dice que no
 
+verdadero = True
+falso = False
 
+print('verdadero =', verdadero, '|', not verdadero)
+print('falso =', falso, '|', not falso)
+
+pregunta = 10 < 5 # False
+print(pregunta)
+print(not pregunta)
+
+print()
+print( True == True ) # True
+print( False == False ) # True
+print( True == False ) # False
+print( False == True ) # False
+print( True == (not False) ) # True #! no olvidar el paréntesis 
+
+
+# * negación de números?
 
 # -----------------------------------------------------
 # => RECORDAR el valor booleano de las variables
@@ -27,13 +45,31 @@ print('\n1) Operador not')
 # cualquier otro número positivo o negativo => True
 # -----------------------------------------------------
 
+print(5)
+print(not 5) # False
+print(not 0) # True
 
+# -, + diferente de 0 => True
+# 0 => False
+
+print(not -20) # False
+
+
+
+# * negando un string?
 
 # ----------------------------------------------------
 # => RECORDAR el valor booleano de las variables
 # string vacío => False
 # cualquier otro string así sea un espacio => True
 # ----------------------------------------------------
+
+cadena_1 = 'cesar'
+cadena_2 = ' '
+cadena_3 = '' # False => True
+
+print( cadena_1, cadena_2, cadena_3 ) # cesar
+print( not cadena_1, not cadena_2, not cadena_3 ) # False False True
 
 
 
@@ -43,12 +79,32 @@ print('\n2) Operador and')
 #  - viene de la palabra conjunto => unido, contiguo, cercano
 #  - agrupa valores booleanos UNIENDO
 #  ! unión lógica (Y)
-#  - basta que uno sea False => el resultado es False
+#  ! basta que uno sea False => el resultado es False
 
 print('\nTabla lógica de and (Y)\n')
 
+print( 'True and True =' , True and True ) # True and True = True
+print( 'True and False =' , True and False ) # True and False = False
+print( 'False and True =' , False and True ) # False and True = False
+print( 'False and False =' , False and False ) # False and False = False
+
 
 print('\nEjemplo práctico => analizar número en rango\n')
+
+temp_1 = 25
+temp_2 = 10
+temp_3 = 40
+# => si la temperatura está entre 20 y 30 ==> buen clima !!
+
+# => vamos a mezclar / combinar
+# operadores lógicos + operadores de comparación
+
+print('TEMP 1 =', temp_1, '¿buen clima? =>', temp_1 >= 20 and temp_1 <= 30)
+print('TEMP 2 =', temp_2, '¿buen clima? =>', temp_2 >= 20 and temp_2 <= 30)
+print('TEMP 3 =', temp_3, '¿buen clima? =>', temp_3 >= 20 and temp_3 <= 30)
+# TEMP 1 = 25 ¿buen clima? => True
+# TEMP 2 = 10 ¿buen clima? => False
+# TEMP 3 = 40 ¿buen clima? => False
 
 
 print('\nEjemplo práctico => comprobar una cadena de texto\n')
@@ -57,44 +113,122 @@ nombre_user_1 = 'Sebas'
 nombre_user_2 = 'dan'
 
 # comprobar:
-# - al menos 5 caracteres
 # - que empiece con mayúscula
+# - al menos 5 caracteres
 # - que sean sólo letras
 
 # comprobación 1
+print( nombre_user_1.istitle(),
+       len(nombre_user_1) >= 5,
+       nombre_user_1.isalpha()  ,
+       '¿Es válido? =>',
+       nombre_user_1.istitle() and len(nombre_user_1) >= 5 and nombre_user_1.isalpha()
+     )
 
 
 # comprobación 2
+print( nombre_user_2.istitle(),
+       len(nombre_user_2) >= 5,
+       nombre_user_2.isalpha()  ,
+       '¿Es válido? =>',
+       nombre_user_2.istitle() and len(nombre_user_2) >= 5 and nombre_user_2.isalpha()
+     )
 
 
-
-#? 3) Operador and
-print('\n3) Operador and')
+#? 3) Operador or
+print('\n3) Operador or')
 #  - operador de disyunción
 #  - viene de la palabra disyunto => separado, apartado, distante
 #  - agrupa valores booleanos SEPARANDO
 #  ! separación lógica (O) 
-#  - basta que uno sea True => el resultado es True
+#  ! basta que uno sea True => el resultado es True
 
 print('\nTabla lógica de or (O)\n')
 
+print( 'True or True =' , True or True ) # True or True = True
+print( 'True or False =' , True or False ) # True or False = True
+print( 'False or True =' , False or True ) # False or True = True
+print( 'False or False =' , False or False ) # False or False = False
 
 
 print('\nEjemplo práctico => analizar número en rango\n')
 # una medida debe estar entre 12 y 15
 # si no está, entonces está fuera del rango
 
+m_1 = 10
+m_2 = 13
+m_3 = 15
+m_4 = 50
+
+print('¿Están las medidas FUERA del rango?')
+#! un mejor ejemplo para or !!
+print( 'M1 =', m_1, '==>', m_1 < 12 or m_1 > 15 ) # M1 = 10 ==> True
+print( 'M2 =', m_2, '==>', m_2 < 12 or m_2 > 15 ) # M2 = 13 ==> False
+print( 'M3 =', m_3, '==>', m_3 < 12 or m_3 > 15 ) # M3 = 15 ==> False
+print( 'M4 =', m_4, '==>', m_4 < 12 or m_4 > 15 ) # M4 = 50 ==> True
+
 
 
 print('\nEjemplo práctico => analizar comandos en un programa\n')
-# tenemos 3 comandos posibles
+# tenemos SÓLO 3 comandos posibles
 # - añadir
 # - eliminar
 # - salir
 
+com_1 = 'añadir' #*
+com_2 = 'hola'
+com_3 = 'eliminar' #*
+com_4 = 'mi_nombre'
+com_5 = 'salir' #*
+
+print('¿Existe el comando en el programa?')
+print(com_1, '? =>',
+      com_1 == 'añadir' or com_1 == 'eliminar' or com_1 == 'salir')
+
+print(com_2, '? =>',
+      com_2 == 'añadir' or com_2 == 'eliminar' or com_2 == 'salir')
+
+print(com_3, '? =>',
+      com_3 == 'añadir' or com_3 == 'eliminar' or com_3 == 'salir')
+
+print(com_4, '? =>',
+      com_4 == 'añadir' or com_4 == 'eliminar' or com_4 == 'salir')
+
+print(com_5, '? =>',
+      com_5 == 'añadir' or com_5 == 'eliminar' or com_5 == 'salir')
+
+# TEST CONSOLA
+# ----------------
+# añadir ? => True
+# hola ? => False
+# eliminar ? => True
+# mi_nombre ? => False
+# salir ? => True
 
 
+# ----------------------------------------------
 #! Salto en el tiempo
 # => veremos que esto es más fácil con listas
-# utilizando palabra clave in
+# utilizando palabra clave / keyword => in
+# ----------------------------------------------
+
 print()
+
+com_1 = 'añadir' #*
+com_2 = 'hola'
+com_3 = 'eliminar' #*
+com_4 = 'mi_nombre'
+com_5 = 'salir' #*
+
+lista_comandos = ['añadir', 'eliminar', 'salir']
+
+# EXPLICACIÓN
+# => al escribir esto:
+# com_1 in lista_comandos
+# ==> ¿existe com_1 dentro de la lista_comandos?
+
+print( com_1, '?? =>', com_1 in lista_comandos ) # añadir ?? => True
+print( com_2, '?? =>', com_2 in lista_comandos ) # hola ?? => False
+print( com_3, '?? =>', com_3 in lista_comandos ) # eliminar ?? => True
+print( com_4, '?? =>', com_4 in lista_comandos ) # mi_nombre ?? => False
+print( com_5, '?? =>', com_5 in lista_comandos ) # salir ?? => True
--- 22-keyword-in/keyword_in.py
+++ 22-keyword-in/keyword_in.py
@@ -1,5 +1,5 @@
 # ========================================================================
-# Palabra Clave / Palabra Reservada => in
+# Palabra Clave / Palabra Reservada / Keyword => in
 
 # - para comprobar si un valor está dentro de una secuencia
 # - devuelve un booleano => True / False
@@ -12,23 +12,59 @@
 #? 1) "in" + string
 print('\n1) "in" + string')
 
+cadena_1 = 'python'
+
+print()
+print( 'm' in cadena_1 ) # False
+print( 'o' in cadena_1 ) # True
+print( 'P' in cadena_1 ) # False
+print( 'y' in cadena_1 ) # True
+print( 'N' in cadena_1 ) # False
+
+print()
+
+print( 'h' in 'hola' ) # True
+
 
 
 #? 2) "in" + lista
 print('\n2) "in" + lista')
 
-lista = [100,200,True,-8.5,'nombre']
+lista = [100, 200, True, -8.5, 'nombre']
+
+print()
+print( 100 in lista ) # True
+print( '100' in lista ) # False
+print( 1 in lista ) # True
+print( False in lista ) # False
 
 
 
 lista = ['','a','b']
 
+print()
+print( True in lista ) # False
+print( False in lista ) # False
 
 
 lista = [5,6,7,0]
 
+print()
+print( True in lista ) # False
+print( False in lista ) # True / #! 0 => False
+
 
 lista = [100,200,300]
+
+print()
+print( True in lista ) # False
+print( False in lista ) # False
+
+lista = [100,200,300, 1]
+
+print()
+print( True in lista ) # True / #! 1= > True
+print( False in lista ) # False
 
 
 
@@ -39,14 +75,25 @@ print('\n3) SALTO EN EL TIEMPO')
 # - pero el uso más común de for es para recorrer un elemento iterable
 # - elementos iterables por el momento: listas, strings
 
-nombre = 'sebas'
-autos = ['Vitara', 'BMW', 'Ford', 'Ferrari']
+nombre = 'sebas' # str
+autos = ['Vitara', 'BMW', 'Ford', 'Ferrari'] # list
 
 # => recorrer un string
+for x in nombre:
+    print(x)
 
+print('------')
+print(x) # al utilizar for, la variable con la que iteramos se crea
 
 # => recorrer una lista
+print()
 
+for x in autos:
+    print(x)
+
+print('------')
+print(x)
+print()
 
 # - x, el nombre de la variable a recorrer es por el momento arbitrario
 # - puede ser cualquier nombre
@@ -54,3 +101,9 @@ autos = ['Vitara', 'BMW', 'Ford', 'Ferrari']
 # - cuando se usa un bucle, condicional, etc, se crea un nuevo bloque de código
 # - por el momento esto solo es una introducción muy breve
 # - ya veremos este tema a profundidad
+
+for n in nombre:
+    print(n)
+    
+for auto in autos:
+    print(auto)
