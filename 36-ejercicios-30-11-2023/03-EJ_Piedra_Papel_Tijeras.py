8d059a9012855cbbdcd0665401ac8c48b34e84c1
Sebastian Silva <sebas.silva.p@gmail.com>
2023-11-30 17:56:38 +0100

    Update Archivos - 30.11.2023-17:56pm

---
+++ 36-ejercicios-30-11-2023/03-EJ_Piedra_Papel_Tijeras.py
@@ -0,0 +1,8 @@
+import random
+
+# ================
+# Ejemplo de uso
+# ================
+# random.shuffle() => me baraja la lista
+# random.choice() => selecciona al azar elemento de lista
+
---
+++ 36-ejercicios-30-11-2023/04-EJ_Ecuaciones_3_x_3.py
@@ -0,0 +1,20 @@
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+
+# ? EJERCICIO
+"""
+2x - 3y + z = 15
+-5x + 8y - 6z = -20
+2x + 7y - 15z = 14
+
+x, y, z => ?
+"""
+# - Realizar un programa en Python
+# - Para calcular los valores de x, y, z
+
+# FUENTES
+# - https://es.wikipedia.org/wiki/Regla_de_Cramer
+
+# - http://matematicas.relatividad.org/widget-sistema-tres-incognitas.html
+
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+
